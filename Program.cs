7230a96b826e9f77bde0d103196accf0591bf7f3
Sharad Singh <sharadthemastersingh@gmail.com>
2025-07-04 16:32:27 +0530

    Added error logging for startup crash

--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
+using GanpatiPaymentsAPI.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.WebHost.ConfigureKestrel(options =>
@@ -7,12 +10,26 @@ builder.WebHost.ConfigureKestrel(options =>
 
 builder.Services.AddControllers();
 
+// Add your DB connection config here if not added yet
+builder.Services.AddDbContext<PaymentsDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("PostgreSqlConnection")));
+
 var app = builder.Build();
 
-app.UseHttpsRedirection();
-app.UseAuthorization();
-app.MapControllers();
-app.Run();
+try
+{
+    app.UseHttpsRedirection();
+    app.UseAuthorization();
+    app.MapControllers();
+    app.Run();
+}
+catch (Exception ex)
+{
+    Console.WriteLine("Application failed to start: " + ex.Message);
+    Console.WriteLine(ex.StackTrace);
+    throw;
+}
+
 
 /*
 using GanpatiPaymentsAPI.Data;
